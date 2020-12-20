8ec5a1f1ebf878bba4c35ee82b7427357144cc1c
Dennis Koech <koechdennis031@gmail.com>
2020-12-20 12:36:19 +0300

    Joji

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -33,7 +33,12 @@
           {{ config('app.name', 'Laravel') }}
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-          <span class="navbar-toggler-icon"><i class="bi bi-chevron-double-down"></i></span>
+          <span class="navbar-toggler-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+              <path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+          </svg>  
+          </span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
